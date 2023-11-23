53373596406596c7d661005da1831c577611414a
Prioravod <Prioravod@github.com>
2023-11-23 04:52:01 +0400

    Initial commit

---
+++ src/main/java/ru/medicine/cardiowavemonitor/CardiowaveMonitorApplication.java
@@ -0,0 +1,13 @@
+package ru.medicine.cardiowavemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardiowaveMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardiowaveMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/medicine/cardiowavemonitor/CardiowaveMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.medicine.cardiowavemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardiowaveMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
